659d252cdda148b5a0b8cadde0caa4af8e133442
Jonathan Opperman <jonathan.opperman@thesustainables.nl>
2023-09-16 18:23:28 +0200

    Added dummy data

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-// In-memory storage for counters
-const counters = {};
+// In-memory storage for counters with dummy data
+const counters = {
+  'namespace1/key1': { count: 10, timestamp: new Date() },
+  'namespace1/key2': { count: 5, timestamp: new Date() },
+  'namespace2/key3': { count: 15, timestamp: new Date() },
+};
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is the root route!');
+});
 
 // Endpoint to increment a counter
 app.get('/hit/:namespace/:key', (req, res) => {
@@ -14,22 +22,25 @@ app.get('/hit/:namespace/:key', (req, res) => {
   const counterKey = `${namespace}/${key}`;
 
   if (!counters[counterKey]) {
-    counters[counterKey] = 1;
-  } else {
-    counters[counterKey]++;
+    // Counter does not exist, return an error response
+    return res.status(404).json({ error: 'Counter not found' });
   }
 
-  res.json({ value: counters[counterKey] });
+  // Counter exists, increment it
+  counters[counterKey].count++;
+  counters[counterKey].timestamp = new Date(); // Update the timestamp
+
+  res.json({ value: counters[counterKey].count, timestamp: counters[counterKey].timestamp });
 });
 
-// Endpoint to get the value of a counter
+// Endpoint to get the value and timestamp of a counter
 app.get('/get/:namespace/:key', (req, res) => {
   const { namespace, key } = req.params;
   const counterKey = `${namespace}/${key}`;
 
-  const value = counters[counterKey] || 0;
+  const { count, timestamp } = counters[counterKey] || { count: 0, timestamp: null };
 
-  res.json({ value });
+  res.json({ value: count, timestamp });
 });
 
 // Start the server
